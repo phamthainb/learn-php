d5ec13065818fe2e4fef4c3be83bd8a7a77336a5
phamthainb <phamthai4643@gmail.com>
2020-02-11 16:56:56 +0700

    init demo_code

---
+++ application/views/demo.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+
+	<style type="text/css">
+
+	</style>
+</head>
+<body>
+
+<div id="container">
+	<h2>
+		demo view lalala
+		<?php echo $ds ?>
+	</h2>
+</div>
+
+</body>
+</html>
